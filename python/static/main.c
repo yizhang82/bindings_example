8e48eeba58a230e7d82c246c239ed2c9f1396e71
Yi Zhang <yizhang82@outlook.com>
2018-01-13 22:47:20 -0800

    Python/C extension with only static methods

---
+++ python/static/main.c
@@ -0,0 +1,30 @@
+
+#define MS_NO_COREDLL
+#include <python.h>
+
+static PyObject *FastInt_Add(PyObject *self, PyObject *args)
+{
+    int a, b;
+    if (!PyArg_ParseTuple(args, "ii", &a, &b))
+    {
+        return NULL;
+    }
+
+    return Py_BuildValue("i", a + b);
+}
+
+static PyMethodDef FastInt_StaticMethods[] = {
+    { "add", FastInt_Add, METH_VARARGS, "Add two integers" },
+    { NULL, NULL, 0, NULL }
+};
+
+#ifdef _MSC_VER
+#define PYMODINIT_FUNC __declspec(dllexport) void
+#else
+#define PYMODINIT_FUNC void
+#endif
+
+PYMODINIT_FUNC initFastInt()
+{
+    Py_InitModule("FastInt", FastInt_StaticMethods);
+}
