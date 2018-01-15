a245bae8b3d54295fcc7843cde51cd04e2639eaf
Yi Zhang <yizhang82@outlook.com>
2018-01-14 21:01:20 -0800

    Python extension module should be named .pyd

--- python/static/main.c
+++ python/static/main.c
@@ -1,5 +1,3 @@
-
-#define MS_NO_COREDLL
 #include <python.h>
 
 static PyObject *FastInt_Add(PyObject *self, PyObject *args)
@@ -24,7 +22,7 @@ static PyMethodDef FastInt_StaticMethods[] = {
 #define PYMODINIT_FUNC void
 #endif
 
-PYMODINIT_FUNC initFastInt()
+PYMODINIT_FUNC initfastint()
 {
-    Py_InitModule("FastInt", FastInt_StaticMethods);
+    Py_InitModule("fastint", FastInt_StaticMethods);
 }
